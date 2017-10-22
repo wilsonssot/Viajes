1d4e351d630d194663f7031f93f37f0ddf7b3bf2
wilsonssot <wilsonss_emelec@hotmail.com>
2017-10-21 23:37:11 -0500

    Mejorando Holamundox2

--- Desarrollo/src/desarrollo/HolaMundox2.java
+++ Desarrollo/src/desarrollo/HolaMundox2.java
@@ -1,6 +1,9 @@
-public static void main (String[] args){
-  System.out.println("Hola Mundo x2");
-  System.out.println("c mamut");
+public class  HolaMundox2{
   
+  public static void main (String[] args){
+    System.out.println("Hola Mundo x2");
+    System.out.println("c mamut");
 
+
+  }
 }
